a6a117bdac2ccc492a371183cf64b6ee19d1aea7
jealousmango@gmail.com <jealousmango@gmail.com>
2018-02-09 04:00:57 +0000

    2 LED's working with button

--- buttonPress.py
+++ buttonPress.py
@@ -1,6 +1,20 @@
 import RPi.GPIO as GPIO
+import time
 
-# Configure pins so that the Pi knows that they are inputs
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5 GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-print(GPIO.input(5))
+GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
+while True:
+	input_state = GPIO.input(4)
+	if input_state == False:
+		print('Button is pressed')
+		GPIO.output(18, GPIO.HIGH)
+		GPIO.output(26, GPIO.HIGH)
+		time.sleep(0.2)
+	else:
+		GPIO.output(18, GPIO.LOW)
+		GPIO.output(26, GPIO.LOW)
+
+GPIO.cleanup()
