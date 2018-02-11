ee085a86f3a094a82627c5c7b9df64ce4ce9b926
jealousmango@gmail.com <jealousmango@gmail.com>
2018-02-11 06:09:47 +0000

    Button press changes.

--- buttonPress.py
+++ buttonPress.py
@@ -3,18 +3,38 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
 GPIO.setup(26, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+
+print ('Let there be light.')
+GPIO.output(12, GPIO.HIGH)
+GPIO.output(18, GPIO.HIGH)
+GPIO.output(24, GPIO.HIGH)
+GPIO.output(25, GPIO.HIGH)
+GPIO.output(26, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
 
 while True:
 	input_state = GPIO.input(4)
 	if input_state == False:
 		print('Button is pressed')
+		GPIO.output(12, GPIO.HIGH)
 		GPIO.output(18, GPIO.HIGH)
+		GPIO.output(24, GPIO.HIGH)
+		GPIO.output(25, GPIO.HIGH)
 		GPIO.output(26, GPIO.HIGH)
+		GPIO.output(27, GPIO.HIGH)
 		time.sleep(0.2)
 	else:
 		GPIO.output(18, GPIO.LOW)
 		GPIO.output(26, GPIO.LOW)
+		GPIO.output(24, GPIO.LOW)
+		GPIO.output(12, GPIO.LOW)
+		GPIO.output(27, GPIO.LOW)
+		GPIO.output(25, GPIO.LOW)
 
 GPIO.cleanup()
