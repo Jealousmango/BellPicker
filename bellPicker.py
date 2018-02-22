4de3b7d236e0769812b25f22a8104302ec200741
Jealousmango <jealousmango@gmail.com>
2018-02-22 08:40:25 -0800

    Update bellPicker.py
    Calling slackHandler.

--- bellPicker.py
+++ bellPicker.py
@@ -78,6 +78,8 @@ def slackHandler():
                         text="Selecting a winner...",
                         as_user=True)
                     main()
+                    
+slackHandler()
 
 def chooseWinner():
     print('Choosing winner...')
