e5eaaee0614166c63bfb5372930d73b1f45c3163
Jealousmango <jealousmango@gmail.com>
2018-02-22 08:36:54 -0800

    Update bellPicker.py

--- bellPicker.py
+++ bellPicker.py
@@ -33,12 +33,12 @@ slack_client = SlackClient(config.api_key)
 user_list = slack_client.api_call("users.list")
 
 fantasticGifs = ["https://giphy.com/gifs/excited-the-office-celebrate-Is1O1TWV0LEJi",
-				"https://giphy.com/gifs/news-atl-down-MTclfCr4tVgis",
-				"https://giphy.com/gifs/cheer-cheering-oxW9IXKWP2Ouk",
-				"https://giphy.com/gifs/internet-eoxomXXVL2S0E",
-				"https://giphy.com/gifs/cat-reaction-youtubers-609o8uNjasiJO",
-				"https://giphy.com/gifs/reactionseditor-come-on-you-got-this-3og0IPMeREHpEV0f60",
-				"https://giphy.com/gifs/heyarnold-nickelodeon-hey-arnold-26Ff4Ci2RNT1H1zb2"]
+                "https://giphy.com/gifs/news-atl-down-MTclfCr4tVgis",
+                "https://giphy.com/gifs/cheer-cheering-oxW9IXKWP2Ouk",
+                "https://giphy.com/gifs/internet-eoxomXXVL2S0E",
+                "https://giphy.com/gifs/cat-reaction-youtubers-609o8uNjasiJO",
+                "https://giphy.com/gifs/reactionseditor-come-on-you-got-this-3og0IPMeREHpEV0f60",
+                "https://giphy.com/gifs/heyarnold-nickelodeon-hey-arnold-26Ff4Ci2RNT1H1zb2"]
 # Used to ping winner.
 winnerNames = ["@lizl", "@charles.mitchell", "@shuggard", "@hubert-j-farnsworth", "@qwerji", "@eddrakee"]
 
@@ -51,112 +51,112 @@ if slack_client.rtm_connect():
 
 
 def main():
-	turnOffLeds()
-	chooseWinner()
+    turnOffLeds()
+    chooseWinner()
 
 def turnOffLeds():
-	GPIO.output(24, GPIO.LOW)
-	GPIO.output(18, GPIO.LOW)
-	GPIO.output(26, GPIO.LOW)
-	GPIO.output(12, GPIO.LOW)
-	GPIO.output(27, GPIO.LOW)
-	GPIO.output(25, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)
 
-def chooseWinner():
-	print('Choosing winner...')
-	# Amount of time to sleep between LEDs.
-	timeToWait = .5
-	# Capture the index that "wins"
-	winningIndex = random.randint(0, len(contestants) - 1)
-	winnerHandle = winnerNames[winningIndex]
-	print("winningIndex is: ", winningIndex)
-	# Save the value of the winning index and then remove it from the list.
-	winner = contestants[winningIndex]
-	contestants.remove(winner)
-	# Loop through remaining "losers".
-	for x in range(0, len(contestants)):
-		# Light each losing pin.
-		GPIO.output(contestants[x], GPIO.HIGH)
-		print("Lighting index: ", contestants[x])
-		time.sleep(timeToWait)
-		# Turn off losing pin to prepare for the next pin to be lit.
-		GPIO.output(contestants[x], GPIO.LOW)
-		# Increase the amount of time to wait between pins.
-		timeToWait = timeToWait * 1.25
-	# Light up the winning pin.
-	# GPIO.output(winner, GPIO.HIGH)
-	for blinks in range(0, 10):
-		GPIO.output(winner, GPIO.HIGH)
-		print("Blinking!")
-		time.sleep(1)
-		GPIO.output(winner, GPIO.LOW)
-		time.sleep(1)
-	GPIO.output(winner, GPIO.HIGH)
-	winningMessage = winnerHandle
-	slack_client.api_call(
-                    "chat.postMessage",
-                    channel = "alert",
-                    text = "The winner has been selected...",
-                    as_user = True)
-
-	time.sleep(1)
-
-	slack_client.api_call(
-                    "chat.postMessage",
-                    channel = "alert",
-                    text = winningMessage,
-                    as_user = True,
-                    link_names = True)
-	fantasticGifToPost = random.randint(0, len(fantasticGifs) - 1)
-
-	slack_client.api_call(
-                    "chat.postMessage",
-                    channel = "alert",
-                    text = "You must answer the call of duty!",
-                    as_user = True)
-
-	slack_client.api_call(
-                    "chat.postMessage",
-                    channel = "alert",
-                    text = fantasticGifs[fantasticGifToPost],
-                    as_user = True)
-# Listen for a "ding" message.
-while True:
-    for message in slack_client.rtm_read():
-        if "text" in message and message ["text"].startswith("<@%s>" % slack_user_id):
-            print("Message received: %s" % json.dumps(message, indent = 2))
-            message_text = message['text'].\
-                    split("<@%s>" % slack_user_id)[1].\
+def slackHandler():
+    while True:
+        for message in slack_client.rtm_read():
+            if "text" in message and message["text"].startswith("<@%s>" % slack_user_id):
+                print("Message received: %s" % json.dumps(message, indent=2))
+                message_text = message['text']. \
+                    split("<@%s>" % slack_user_id)[1]. \
                     strip()
 
-        if re.match(r'.*(ding).*', message_text, re.IGNORECASE):
-            slack_client.api_call(
-                "chat.postMessage",
-                channel = "general",
-                text = "Selecting a winner...",
-                as_user = True)
-			main()
+                if re.match(r'.*(ding).*', message_text, re.IGNORECASE):
+                    slack_client.api_call(
+                        "chat.postMessage",
+                        channel="general",
+                        text="Selecting a winner...",
+                        as_user=True)
+                    main()
+
+def chooseWinner():
+    print('Choosing winner...')
+    # Amount of time to sleep between LEDs.
+    timeToWait = .5
+    # Capture the index that "wins"
+    winningIndex = random.randint(0, len(contestants) - 1)
+    winnerHandle = winnerNames[winningIndex]
+    print("winningIndex is: ", winningIndex)
+    # Save the value of the winning index and then remove it from the list.
+    winner = contestants[winningIndex]
+    contestants.remove(winner)
+    # Loop through remaining "losers".
+    for x in range(0, len(contestants)):
+        # Light each losing pin.
+        GPIO.output(contestants[x], GPIO.HIGH)
+        print("Lighting index: ", contestants[x])
+        time.sleep(timeToWait)
+        # Turn off losing pin to prepare for the next pin to be lit.
+        GPIO.output(contestants[x], GPIO.LOW)
+        # Increase the amount of time to wait between pins.
+        timeToWait = timeToWait * 1.25
+    # Light up the winning pin.
+    # GPIO.output(winner, GPIO.HIGH)
+    for blinks in range(0, 10):
+        GPIO.output(winner, GPIO.HIGH)
+        print("Blinking!")
+        time.sleep(1)
+        GPIO.output(winner, GPIO.LOW)
+        time.sleep(1)
+    GPIO.output(winner, GPIO.HIGH)
+    winningMessage = winnerHandle
+    slack_client.api_call(
+        "chat.postMessage",
+        channel = "alert",
+        text = "The winner has been selected...",
+        as_user = True)
+
+    time.sleep(1)
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel = "alert",
+        text = winningMessage,
+        as_user = True,
+        link_names = True)
+    fantasticGifToPost = random.randint(0, len(fantasticGifs) - 1)
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel = "alert",
+        text = "You must answer the call of duty!",
+        as_user = True)
+
+    slack_client.api_call(
+        "chat.postMessage",
+        channel = "alert",
+        text = fantasticGifs[fantasticGifToPost],
+        as_user = True)
 
 # while end != True:
 while True:
-	# Listen for button press.
-	input_state = GPIO.input(4)
-	if input_state == False:
-		print("Button has been pressed.")
-		main()
-		print("Nap")
-		time.sleep(10)
-		# end = True
-		# Shut it down.
-		turnOffLeds()
-		contestants = [12, 18, 24, 25, 26, 27]
-	else:
-		GPIO.output(18, GPIO.LOW)
-		GPIO.output(26, GPIO.LOW)
-		GPIO.output(24, GPIO.LOW)
-		GPIO.output(12, GPIO.LOW)
-		GPIO.output(27, GPIO.LOW)
-		GPIO.output(25, GPIO.LOW)
+    # Listen for button press.
+    input_state = GPIO.input(4)
+    if input_state == False:
+        print("Button has been pressed.")
+        main()
+        print("Nap")
+        time.sleep(10)
+        # end = True
+        # Shut it down.
+        turnOffLeds()
+        contestants = [12, 18, 24, 25, 26, 27]
+    else:
+        GPIO.output(18, GPIO.LOW)
+        GPIO.output(26, GPIO.LOW)
+        GPIO.output(24, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(27, GPIO.LOW)
+        GPIO.output(25, GPIO.LOW)
 
 # GPIO.cleanup()
-print("Jobs done!")
