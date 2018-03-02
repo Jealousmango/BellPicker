d0109120ef3376f66dfe1658bd433f366963da81
jealousmango <jealousmango@gmail.com>
2018-03-02 06:21:02 -0800

    All 6 pins together now.

--- breathingLed.py
+++ breathingLed.py
@@ -1,38 +1,79 @@
 import time
 import RPi.GPIO as GPIO
 
-LedPin = 18
+# LedPin = 18
 
 def setup():
-    global p
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(LedPin, GPIO.OUT)
-    GPIO.output(LedPin, GPIO.LOW)
+    global twelve
+    global eighteen
+    global twentyFour
+    global twentyFive
+    global twentySix
+    global twentySeven
 
-    p = GPIO.PWM(LedPin, 1000)
-    p.start(0)
+    GPIO.setmode(GPIO.BOARD)
+    # GPIO.setup(LedPin, GPIO.OUT)
+    GPIO.setup(12, GPIO.OUT)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+    GPIO.setup(25, GPIO.OUT)
+    GPIO.setup(26, GPIO.OUT)
+    GPIO.setup(27, GPIO.OUT)
+
+    # GPIO.output(LedPin, GPIO.LOW)
+    GPIO.output(18, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
+    GPIO.output(24, GPIO.LOW)
+    GPIO.output(12, GPIO.LOW)
+    GPIO.output(27, GPIO.LOW)
+    GPIO.output(25, GPIO.LOW)
+
+    twelve = GPIO.PWM(12, 1000)
+    eighteen = GPIO.PWM(12, 1000)
+    twentyFour = GPIO.PWM(12, 1000)
+    twentyFive = GPIO.PWM(12, 1000)
+    twentySix = GPIO.PWM(12, 1000)
+    twentySeven = GPIO.PWM(12, 1000)
+    twelve.start(0)
+
+    twelve.start(0)
+    eighteen.start(0)
+    twentyFour.start(0)
+    twentyFive.start(0)
+    twentySix.start(0)
+    twentySeven.start(0)
 
 def loop():
     while True:
         for dc in range(0, 101, 4):
-            p.ChangeDutyCycle(dc)
+            twelve.ChangeDutyCycle(dc)
+            eighteen.ChangeDutyCycle(dc)
+            twentyFour.ChangeDutyCycle(dc)
+            twentyFive.ChangeDutyCycle(dc)
+            twentySix.ChangeDutyCycle(dc)
+            twentySeven.ChangeDutyCycle(dc)
             time.sleep(0.05)
         time.sleep(1)
         for dc in range(100, -1, -4):
-            p.ChangeDutyCycle(dc)
+            twelve.ChangeDutyCycle(dc)
+            twelve.ChangeDutyCycle(dc)
+            eighteen.ChangeDutyCycle(dc)
+            twentyFour.ChangeDutyCycle(dc)
+            twentyFive.ChangeDutyCycle(dc)
+            twentySix.ChangeDutyCycle(dc)
+            twentySeven.ChangeDutyCycle(dc)
             time.sleep(0.05)
         time.sleep(1)
 
 def destroy():
-    p.stop()
-    GPIO.output(LedPin, GPIO.HIGH)
+    twelve.stop()
+    GPIO.output(12, GPIO.HIGH)
+    GPIO.output(18, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
+    GPIO.output(24, GPIO.HIGH)
+    GPIO.output(27, GPIO.HIGH)
+    GPIO.output(25, GPIO.HIGH)
     GPIO.cleanup()
-
-
-def destroy():
-	p.stop()
-	GPIO.output(LedPin, GPIO.HIGH)    # turn off all leds
-	GPIO.cleanup()
 
 if __name__ == '__main__':     # Program start from here
 	setup()
